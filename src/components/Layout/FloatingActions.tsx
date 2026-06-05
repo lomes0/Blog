78cc443a17fa4e6195ef54d1734abd0fc68ca230
eran.sbt0 <eran.sbt0@gmail.com>
2026-06-05 21:12:03 +0300

    remove footer floating action with version, update, and privacy links

--- src/components/Layout/FloatingActions.tsx
+++ src/components/Layout/FloatingActions.tsx
@@ -1,22 +1,11 @@
 "use client";
 import { FloatingActionsContainer } from "./FloatingActionsContainer";
-import FooterWithFloatingAction from "../Home/FooterWithFloatingAction";
 import { ReactNode } from "react";
-
-export function FloatingActions() {
-  return (
-    <>
-      <FooterWithFloatingAction />
-      {/* Additional floating action buttons can be added here */}
-    </>
-  );
-}
 
 export function FloatingActionsProvider({ children }: { children: ReactNode }) {
   return (
     <FloatingActionsContainer>
       {children}
-      <FloatingActions />
     </FloatingActionsContainer>
   );
 }
